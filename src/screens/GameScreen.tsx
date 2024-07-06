9137d46866f503f8cb5dc6a989df22cb0f074c17
dtb4life <aveok4now@gmail.com>
2024-07-06 18:32:56 +0300

    feat: show Confetti on round success

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,12 +1,15 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { BorderBeam } from "../components/client/magic/BorderBeam";
+import Confetti, {
+	type ConfettiRef,
+} from "../components/client/magic/Confetti";
 import { Statistics } from "../components/client/Statistics";
 import { TilesBoard } from "../components/client/TilesBoard";
 import { tiles } from "../config";
 import { useTimer } from "../hooks/useTimer";
 import { cn } from "../lib/utils";
 import type { GameConfig } from "../models/GameConfig";
-import Result from "../screens/ResultsScreen";
+import ResultsScreen from "../screens/ResultsScreen";
 import { GameService } from "../services/GameService";
 import { TimerService } from "../services/TimerService";
 
@@ -20,6 +23,8 @@ export function GameScreen({ config }: GameScreenProps) {
 	const [matchedCount, setMatchedCount] = useState(0);
 	const [isResultShowed, setResultShowed] = useState(false);
 	const [attempt, setAttempt] = useState(1);
+	const [roundPassed, setRoundPassed] = useState(false);
+	const confettiRef = useRef<ConfettiRef>(null);
 
 	const timer = useTimer(() => {
 		setResultShowed(true);
@@ -27,19 +32,27 @@ export function GameScreen({ config }: GameScreenProps) {
 
 	useEffect(() => {
 		timer.refresh(config.startTime);
-	}, []);
+		setRoundPassed(false);
+	}, [config.startTime]);
 
-	function handleSolve() {
+	const handleSolve = useCallback(() => {
 		if (timer.isExpired) return;
 		setMatchedCount(0);
 		setRound((prev) => prev + 1);
+
 		const newStartTime = GameService.calculateNewStartTime(
 			timerStartTime,
 			config
 		);
 		setTimerStartTime(newStartTime);
 		timer.refresh(newStartTime);
-	}
+		setRoundPassed(true);
+		confettiRef.current?.fire({});
+
+		setTimeout(() => {
+			setRoundPassed(false);
+		}, 3000);
+	}, [config, timer, timerStartTime]);
 
 	const handleCloseResults = useCallback(async function () {
 		setResultShowed(false);
@@ -57,14 +70,16 @@ export function GameScreen({ config }: GameScreenProps) {
 		return { totalTime: timer.totalTime, lastRound: round };
 	}, [isResultShowed]);
 
-	const timeStyle = (() => {
-		if (timer.value <= timerStartTime / 5) return "text-red";
-		if (timer.value <= timerStartTime / 2) return "text-yellow";
-		return "text-white";
-	})();
+	const timeStyle = TimerService.getTimeStyle(timer.value, timerStartTime);
 
 	return (
 		<>
+			{roundPassed && (
+				<Confetti
+					ref={confettiRef}
+					className="absolute left-0 top-0 z-0 h-full w-full"
+				/>
+			)}
 			<div
 				className="mx-auto md:w-full max-w-[35rem] sm:w-[95%]  overflow-hidden p-4"
 				id="gameScreen"
@@ -86,18 +101,18 @@ export function GameScreen({ config }: GameScreenProps) {
 					setMatchedCount={setMatchedCount}
 					attempt={attempt}
 				/>
-				{!isResultShowed && (
-					<Statistics className="mt-4 text-white">
-						{matchedCount + " / " + config.couples}
-					</Statistics>
-				)}
+				<Statistics className="mt-4 text-white">
+					{matchedCount + " / " + config.couples}
+				</Statistics>
 			</div>
 
-			<Result
-				isOpen={isResultShowed}
-				close={handleCloseResults}
-				results={results}
-			/>
+			{timer.isExpired && (
+				<ResultsScreen
+					isOpen={isResultShowed}
+					close={handleCloseResults}
+					results={results}
+				/>
+			)}
 		</>
 	);
 }
